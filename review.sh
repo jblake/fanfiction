e33c6ce86cba5174f0eb62b64ab14e2771baa208
Julian Blake Kongslie <jblake@omgwallhack.org>
2011-10-31 17:35:10 -0700

    Randomize review order; don't open reader.

--- review.sh
+++ review.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for f in $(./find.sh unread); do
+for f in $(./find.sh unread | rl); do
 
   n="$(echo $f | perl -pne 's/.*_([0-9]+)\.epub$/$1/')"
 
-  fbreader import/$f 2>/dev/null
+  #fbreader import/$f 2>/dev/null
+  echo $f
 
   ./tag.sh $n
 
