5c77304a721e4fce00559a7b577e527d7a5478a6
Julian Blake Kongslie <jblake@omgwallhack.org>
2011-11-09 09:46:54 -0800

    Don't columnate if we're part of a pipeline.

--- lstags.sh
+++ lstags.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec tagcoll reverse --derived=DERIVED --items STORIES | column
+if tty -s < /dev/stdout; then
+  COLUMNATE="column"
+else
+  COLUMNATE="cat"
+fi
+
+exec tagcoll reverse --derived=DERIVED --items STORIES | $COLUMNATE
