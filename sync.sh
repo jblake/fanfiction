f90bb0e0341729b9c812dc4ca12c5ca7cf13f276
Julian Blake Kongslie <jblake@omgwallhack.org>
2011-11-09 09:47:30 -0800

    Delete dead stories when syncing.

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@
 echo "Waiting for nook..."
 until ls nook/ > /dev/null 2>&1; do sleep 0.1; done
 
-rsync --inplace --recursive --times --verbose import/ nook/fanfiction/
+rsync --delete --inplace --recursive --times --verbose import/ nook/fanfiction/
