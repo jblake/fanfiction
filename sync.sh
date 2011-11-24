b62d7ebdec1967ab4a9d440eb561a8c123da4ceb
Julian Blake Kongslie <jblake@omgwallhack.org>
2011-11-23 21:23:30 -0800

    Unmount nook when we're done.

--- sync.sh
+++ sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Waiting for nook..."
-until ls nook/ > /dev/null 2>&1; do sleep 0.1; done
+until ls /media/nook/ > /dev/null 2>&1; do sleep 0.1; done
 
 rsync --delete --inplace --recursive --times --verbose import/ nook/fanfiction/
+
+sudo umount /media/nook
