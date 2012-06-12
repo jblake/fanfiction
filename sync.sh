3dff576ac495849645ec0196c9f8bde0e6fab7e3
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-11 19:45:21 -0700

    Initial commit on fanfiction-ng branch.

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Waiting for nook..."
-until ls /media/nook/ > /dev/null 2>&1; do file -s /dev/sdc > /dev/null 2>&1 ; sleep 0.1; done
-
-rsync --delete --inplace --recursive --times --verbose "$@" import/ nook/fanfiction/
-
-sudo umount /media/nook
