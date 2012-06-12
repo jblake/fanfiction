072e554cfa59567151bbc8bf2a779caa64bf5788
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-12 14:42:28 -0700

    Big simplification of ffnet fetcher.

--- src/Fetch/FanfictionNet.hs
+++ src/Fetch/FanfictionNet.hs
@@ -5,27 +5,73 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Fetch.FanfictionNet
+  ( Info(..)
+  , peek
+  , fetch
+  )
 where
 
 import Codec.Text.IConv
+import Control.Monad
 import Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy as BS
 import Data.Maybe
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as T
+import Data.Time.Clock
+import Data.Time.Format
 import Network.Browser
 import Network.HTTP
 import Network.URI
+import System.Locale
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Tree
 
 import EPub
 
-fetch :: String -> String -> IO (Maybe EPub)
-fetch uniqueID storyid = browse $ do
+data Info = Info
+  { infoUnique :: String
+  , infoStoryID :: String
+  , infoTitle :: String
+  , infoAuthor :: String
+  , infoUpdated :: UTCTime
+  , infoChapter :: (String, BS.ByteString)
+  }
+  deriving (Read, Show)
 
-  setAllowRedirects True
+peek :: String -> String -> BrowserAction (HandleStream BS.ByteString) (Maybe Info)
+peek = fetchChapter 1
 
-  (uri, resp) <- request $ mkRequest GET $ fromJust $ parseURI $ "http://m.fanfiction.net/s/" ++ urlEncode storyid ++ "/1"
+fetch :: Info -> BrowserAction (HandleStream BS.ByteString) EPub
+fetch first = do
+
+  let
+    fetchRemain n = do
+
+      i <- fetchChapter n (infoUnique first) (infoStoryID first)
+
+      case i of
+        Nothing -> return []
+        Just next -> do
+
+          remain <- fetchRemain (n + 1)
+
+          return $ infoChapter next : remain
+
+  remain <- fetchRemain 2
+
+  let
+    uniqueID = infoUnique first
+    title = infoTitle first
+    author = infoAuthor first
+    chapters = infoChapter first : remain
+
+  return $ EPub {..}
+
+fetchChapter :: Int -> String -> String -> BrowserAction (HandleStream BS.ByteString) (Maybe Info)
+fetchChapter n infoUnique infoStoryID = do
+
+  (uri, resp) <- request $ mkRequest GET $ fromJust $ parseURI $ "http://m.fanfiction.net/s/" ++ urlEncode infoStoryID ++ "/" ++ urlEncode (show n)
 
   case rspCode resp of
     (2, 0, 0) -> do
@@ -35,44 +81,16 @@ fetch uniqueID storyid = browse $ do
         header = [ t | t@(TagBranch "center" _ _) <- universeTree body ]
         [TagText titleText] = parseTags $ renderTags $ flattenTree $ head [ cs | (TagBranch "b" _ cs) <- universeTree header ]
         [TagText authorText] = parseTags $ renderTags $ flattenTree $ head [ cs | (TagBranch "a" _ cs) <- universeTree header ]
-        title = T.unpack titleText
-        author = T.unpack authorText
         chpTitle = T.strip $ renderTags $ reverse $ takeWhile (~== TagText ("" :: T.Text)) $ dropWhile (~/= TagText ("" :: T.Text)) $ reverse $ flattenTree $ head [ cs | (TagBranch "div" as cs) <- universeTree body, ("id", "content") `elem` as ]
         [TagText chpTitleText] = parseTags chpTitle
         chpContent = T.strip $ renderTags $ flattenTree $ concat [ cs | (TagBranch "div" as cs) <- universeTree body, ("id", "storycontent") `elem` as ]
-        fetchRemain n = do
-
-          (uri, resp) <- request $ mkRequest GET $ fromJust $ parseURI $ "http://m.fanfiction.net/s/" ++ urlEncode storyid ++ "/" ++ urlEncode (show n)
-
-          case rspCode resp of
-            (2, 0, 0) -> do
-
-              let
-                header = [ t | t@(TagBranch "center" _ _) <- universeTree body ]
-                body = tagTree $ parseTags $ T.decodeUtf8 $ convertFuzzy Transliterate "utf-8" "utf-8" $ rspBody resp
-                chpTitle = T.strip $ renderTags $ reverse $ takeWhile (~== TagText ("" :: T.Text)) $ dropWhile (~/= TagText ("" :: T.Text)) $ reverse $ flattenTree $ head [ cs | (TagBranch "div" as cs) <- universeTree body, ("id", "content") `elem` as ]
-                [TagText chpTitleText] = parseTags chpTitle
-                chpContent = T.strip $ renderTags $ flattenTree $ concat [ cs | (TagBranch "div" as cs) <- universeTree body, ("id", "storycontent") `elem` as ]
-
-              case header of
-                [] -> return []
-                _ -> do
-
-                  remain <- fetchRemain $ n + 1
-
-                  return $ (T.unpack chpTitleText, T.encodeUtf8 $ "<?xml version=\"1.0\" encoding=\"utf-8\" ?><html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><meta http-equiv=\"Content-type\" content=\"application/xhtml+xml; charset=utf-8\" /><title>" `T.append` chpTitle `T.append` "</title><style type=\"text/css\">p{text-align:justify;text-justify:newspaper;}</style></head><body>" `T.append` chpContent `T.append` "</body></html>") : remain
-
-            _ -> return []
+        infoTitle = T.unpack titleText
+        infoAuthor = T.unpack authorText
+        infoUpdated = readTime defaultTimeLocale "%m-%d-%y" "1-1-01"
+        infoChapter = (T.unpack chpTitleText, T.encodeUtf8 $ "<?xml version=\"1.0\" encoding=\"utf-8\" ?><html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><meta http-equiv=\"Content-type\" content=\"application/xhtml+xml; charset=utf-8\" /><title>" `T.append` chpTitle `T.append` "</title><style type=\"text/css\">p{text-align:justify;text-justify:newspaper;}</style></head><body>" `T.append` chpContent `T.append` "</body></html>")
 
       case header of
         [] -> return Nothing
-        _ -> do
-
-          remain <- fetchRemain 2
-
-          let
-            chapters = (T.unpack chpTitleText, T.encodeUtf8 $ "<?xml version=\"1.0\" encoding=\"utf-8\" ?><html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><meta http-equiv=\"Content-type\" content=\"application/xhtml+xml; charset=utf-8\" /><title>" `T.append` chpTitle `T.append` "</title><style type=\"text/css\">p{text-align:justify;text-justify:newspaper;}</style></head><body>" `T.append` chpContent `T.append` "</body></html>") : remain
-
-          return $ Just $ EPub {..}
+        _ -> return $ Just $ Info {..}
 
     _ -> return Nothing
