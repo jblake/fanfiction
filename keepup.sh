fa6cef94cd2363dfe05bf2cf0bbc9928ad749c4b
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-11 19:42:23 -0700

    Add some verbosity.

--- keepup.sh
+++ keepup.sh
@@ -2,14 +2,29 @@
 
 ./sync.sh --existing
 
+TOTAL="$(sqlite3 /srv/tags/tags.db "select count(*) from all_items")"
+N=0
+
 sqlite3 /srv/tags/tags.db "select item from all_items" | while read STORY; do
   ./steal.sh "${STORY}" "$@"
   echo
+  echo "$N" "$TOTAL" | dbar
+  echo
+  N="$(($N + 1))"
 done
 
 if [ -e BROKEN ]; then
   echo "Broken stories:"
-  cat BROKEN
+  (
+    cd import
+    cat ../BROKEN | while read STORY; do
+      if [ -e *"_${STORY}.epub" ]; then
+        echo *"_${STORY}.epub"
+      else
+        echo "${STORY}"
+      fi
+    done
+  )
   rm BROKEN
   echo
 fi
