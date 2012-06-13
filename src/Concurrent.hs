8120a11cb770fac0df2230b5196ef6179a7c9dc4
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-12 17:14:41 -0700

    Generalize to support building complex pipelines, which I don't use. :-/

--- src/Concurrent.hs
+++ src/Concurrent.hs
@@ -7,40 +7,52 @@
 module Concurrent
   ( Worker
   , newWorker
-  , defer
-  , immediate
+  , Pipe
+  , first
+  , (|||)
+  , fg
+  , bg
   )
 where
 
 import Control.Concurrent
 import Control.Concurrent.Chan
+import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.IO.Class
-import Network.Browser
 
-data Command c where
-  Command :: Chan a -> BrowserAction c a -> Command c
+data Command m where
+  Command :: (a -> IO ()) -> m a -> Command m
 
-newtype Worker c = Worker (forall a . Chan a -> BrowserAction c a -> IO ())
+newtype Worker m = Worker (Chan (Command m))
 
-newWorker :: IO (Worker c)
-newWorker = do
+newWorker :: (Monad m, MonadIO m) => (m () -> IO ()) -> IO (Worker m)
+newWorker run = do
   commandChan <- newChan
+  forkIO $ run $ forever $ do
+    Command result act <- liftIO $ readChan commandChan
+    act >>= liftIO . result
+  return $ Worker commandChan
 
-  forkIO $ browse $ do
-    setAllowRedirects True
+data Pipe r where
+  Done :: Worker m -> m a -> Pipe a
+  Pass :: Pipe a -> Worker m -> (a -> m b) -> Pipe b
 
-    forever $ do
-      Command responseChan cmd <- liftIO $ readChan commandChan
-      cmd >>= (liftIO . writeChan responseChan)
+first :: Worker m -> m a -> Pipe a
+first = Done
 
-  return $ Worker $ \responseChan cmd -> writeChan commandChan $ Command responseChan cmd
+(|||) :: Pipe a -> Worker m -> (a -> m b) -> Pipe b
+(|||) = Pass
 
-defer :: Worker c -> Chan a -> BrowserAction c a -> IO ()
-defer (Worker worker) = worker
+start :: Pipe r -> (r -> IO ()) -> IO ()
+start (Done      (Worker chan) act) result = writeChan chan $ Command result act
+start (Pass prev (Worker chan) act) result = start prev $ \x -> writeChan chan $ Command result $ act x
 
-immediate :: Worker c -> BrowserAction c a -> IO a
-immediate (Worker worker) cmd = do
-  chan <- newChan
-  worker chan cmd
-  readChan chan
+fg :: Pipe r -> IO r
+fg pipe = do
+  var <- newEmptyMVar
+  start pipe $ putMVar var
+  takeMVar var
+
+bg :: Pipe () -> IO ()
+bg pipe = start pipe $ const $ return ()
