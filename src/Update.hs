7b2b40ada714027380344d38a9f106604b3dad47
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-12 17:15:29 -0700

    Starting work on the updater program.

---
+++ src/Update.hs
@@ -0,0 +1,71 @@
+-- Copyright © 2012 Julian Blake Kongslie <jblake@omgwallhack.org>
+-- Licensed under the MIT license.
+
+module Main
+where
+
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
+import Database.HDBC
+import Database.HDBC.PostgreSQL
+import Network.Browser
+
+import Concurrent
+import EPub
+import Fetch
+import Fetch.FanfictionNet as FFNet
+
+main :: IO ()
+main = do
+
+  ffnetWorker <- newWorker $ \m -> browse $ do
+    setAllowRedirects True
+    setOutHandler $ const $ return ()
+    m
+
+  let
+    tryFetch storyDone storyID [] = do
+      putStrLn $ "No remaining sources for story " ++ storyID ++ "!"
+      putMVar storyDone ()
+
+    tryFetch storyDone storyID (("fanfiction.net", ref):fallbacks) = bg $ first ffnetWorker $ do
+
+        maybeInfo <- FFNet.peek storyID ref
+        case maybeInfo of
+
+          Just info -> liftIO $ bg $ first ffnetWorker $ do
+            epub <- FFNet.fetch info
+            liftIO $ putStrLn $ "Got story " ++ storyID ++ " using fanfiction.net:" ++ ref ++ "."
+            liftIO $ putMVar storyDone ()
+
+          Nothing -> liftIO $ do
+            putStrLn $ "Can't use fanfiction.net:" ++ ref ++ " for story " ++ storyID ++ ": not found!"
+            tryFetch storyDone storyID fallbacks
+
+    tryFetch storyDone storyID ((s, ref):fallbacks) = do
+      putStrLn $ "Can't use source " ++ s ++ ":" ++ ref ++ " for story " ++ storyID ++ ": site not supported!"
+      tryFetch storyDone storyID fallbacks
+
+  signals <- withPostgreSQL "dbname=fanfiction user=fanfiction host=/tmp" $ \db -> do
+
+    unprunedStoriesStmt <- prepare db "select id from stories where not pruned"
+    sourcesStmt <- prepare db "select source, ref from sources where story = ? order by source asc"
+
+    execute unprunedStoriesStmt []
+    rs <- fetchAllRows' unprunedStoriesStmt
+
+    forM [ fromSql idSql | [idSql] <- rs ] $ \storyID -> do
+
+      execute sourcesStmt [toSql storyID]
+      rs <- fetchAllRows' sourcesStmt
+
+      storyDone <- newEmptyMVar
+
+      tryFetch storyDone storyID [ (fromSql sourceSql, fromSql refSql) | [sourceSql, refSql] <- rs ]
+
+      return storyDone
+
+  forM_ signals $ takeMVar
