e707d5d8e14002af0f4527ab1774e09d2d6eff36
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-14 14:32:44 -0700

    Fix the migrate script.

--- migrate.sh
+++ migrate.sh
@@ -4,6 +4,7 @@ set -e
 
 pg_dump -a -F custom -f migrate.dump -U fanfiction fanfiction
 psql -f schema.sql fanfiction fanfiction
-pg_restore -a -F custom -f migrate.dump -U fanfiction -d fanfiction
+pg_restore -a -F custom -U fanfiction -d fanfiction migrate.dump
+psql -c "vacuum full analyze" fanfiction fanfiction
 
 rm -f migrate.dump
