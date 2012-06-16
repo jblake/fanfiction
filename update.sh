c0ac31cde12f3a7571b7a6bb2d4428d5aa3e22f9
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-15 18:17:36 -0700

    Sync with the Nook when updating.

--- update.sh
+++ update.sh
@@ -4,4 +4,10 @@ set -e
 
 ./build.sh
 
+until ls /media/nook/ > /dev/null 2>&1; do file -s /dev/sdc > /dev/null 2>&1 ; sleep 0.1; done
+
+rsync --delete --inplace --recursive --times --verbose --existing /srv/epubs/ "/media/nook/My Files/Books/fanfiction/"
+
 time -p dist/build/update/update +RTS -N -RTS "$@"
+
+rsync --delete --inplace --recursive --times --verbose /srv/epubs/ "/media/nook/My Files/Books/fanfiction/"
