89a8cb64ba03dfa3bf903c9ac9c4ad179a3c0a44
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-06-15 18:50:27 -0700

    Correctness on talking to the nook.

--- update.sh
+++ update.sh
@@ -4,10 +4,12 @@ set -e
 
 ./build.sh
 
-until ls /media/nook/ > /dev/null 2>&1; do file -s /dev/sdc > /dev/null 2>&1 ; sleep 0.1; done
+until ls "/media/nook/My Files" > /dev/null 2>&1; do file -s /dev/sdc > /dev/null 2>&1 || true; sleep 0.1; done
 
 rsync --delete --inplace --recursive --times --verbose --existing /srv/epubs/ "/media/nook/My Files/Books/fanfiction/"
 
 time -p dist/build/update/update +RTS -N -RTS "$@"
 
 rsync --delete --inplace --recursive --times --verbose /srv/epubs/ "/media/nook/My Files/Books/fanfiction/"
+
+sudo umount /media/nook
