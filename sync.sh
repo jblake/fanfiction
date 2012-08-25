897773fa1c0381b77c649ad09fc2592480e0bc0c
Julian Blake Kongslie <jblake@omgwallhack.org>
2012-08-25 10:01:16 -0700

    Add insults while waiting for the Nook.

--- sync.sh
+++ sync.sh
@@ -2,7 +2,30 @@
 
 set -e
 
-until ls "/media/nook/My Files" > /dev/null 2>&1; do file -s /dev/sdc > /dev/null 2>&1 || true; sleep 0.1; done
+file -s /dev/sdc > /dev/null 2>&1 || true
+
+until ls "/media/nook/My Files" > /dev/null 2>&1; do
+
+  file -s /dev/sdc > /dev/null 2>&1 || true
+
+  rl -c 1 << END
+Waiting for Nook to show up...
+You can go ahead an plug the Nook in now.
+Any time, jerk.
+Why don't you plug in the Nook?
+I'm not sure what you're waiting for, but I'm waiting for you.
+The Nook hasn't shown up yet.
+Hey! Plug in the Nook!
+What's the matter? Can't find the cable?
+Sitting around doing nothing is boring.
+*crickets*
+END
+
+  sleep 1
+
+done
+
+echo
 
 rsync --delete --inplace --recursive --times --verbose --existing /srv/epubs/ "/media/nook/My Files/Books/fanfiction/"
 
